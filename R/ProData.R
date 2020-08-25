f0078b6d6018ebb3d41b54a523b38759f217deea
manniealfaro <manniealfaro@gmail.com>
2020-08-25 10:24:59 +0200

    Adds validation for ProData

--- R/ProData.R
+++ R/ProData.R
@@ -8,19 +8,27 @@ setMethod("initialize",
           function(.Object, ..., pro, characteristic, data) {
 
             if(missing(data)) {
-              data = data.frame(0)
+              stop("[ProData: validation] Data must be given")
             }
 
-            headers <- names(data)
+            if(missing(pro)) {
+              stop("[ProData: validation] Proces or pruduct object must be specified")
+            }
+
+            if(missing(pro)) {
+              stop("[ProData: validation] Characteristic object must be specified")
+            }
+
+            callNextMethod(.Object, ..., pro = pro, characteristic = characteristic, data = data)
+
+            headers <- names(.Object@data)
 
             part <- headers[1]
             appraiser <- headers[2]
             variable <- headers[3]
 
-            data[[appraiser]] <- factor(data[[appraiser]])
-            data[[part]] <- factor(data[[part]])
-
-            callNextMethod(.Object, ..., pro = pro, characteristic = characteristic, data = data)
+            .Object@data[[appraiser]] <- factor(.Object@data[[appraiser]])
+            .Object@data[[part]] <- factor(.Object@data[[part]])
           })
 
 setMethod("show", "ProData", function(object) {
